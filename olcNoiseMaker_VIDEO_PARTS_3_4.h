9a12e166a19b41b7b74fe00051c73c827de5c736
Javidx9 <javidx9yt@gmail.com>
2017-11-11 08:40:20 +0000

    Add files via upload
    I was informed that the code was not available for Parts 3&4. It's a bit different so I've kept it separate so newbies can still follow along. Just change the filename.

---
+++ olcNoiseMaker_VIDEO_PARTS_3_4.h
@@ -0,0 +1,284 @@
+/* 
+	OneLoneCoder.com - Simple Audio Noisy Thing
+	"Allows you to simply listen to that waveform!" - @Javidx9
+
+	Disclaimer
+	~~~~~~~~~~
+
+	I don't care what you use this for. It's intended to be educational, and perhaps
+	to the oddly minded - a little bit of fun. Please hack this, change it and use it
+	in any way you see fit. BUT, you acknowledge that I am not responsible for anything 
+	bad that happens as a result of your actions. However, if good stuff happens, I 
+	would appreciate a shout out, or at least give the blog some publicity for me. 
+	Cheers!
+
+	Author
+	~~~~~~
+
+	Twitter: @javidx9
+	Blog: www.onelonecoder.com
+
+	Versions
+	~~~~~~~~
+
+	1.0 - 14/01/17
+	- Controls audio output hardware behind the scenes so you can just focus
+	  on creating and listening to interesting waveforms.
+	- Currently MS Windows only
+
+	Documentation
+	~~~~~~~~~~~~~
+
+	See video: https://youtu.be/tgamhuQnOkM
+
+	This will improve as it grows!
+
+*/
+
+
+#pragma once
+
+#pragma comment(lib, "winmm.lib")
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <atomic>
+#include <condition_variable>
+using namespace std;
+
+#include <Windows.h>
+
+#ifndef FTYPE
+#define FTYPE double
+#endif
+
+const double PI = 2.0 * acos(0.0);
+
+template<class T>
+class olcNoiseMaker
+{
+public:
+	olcNoiseMaker(wstring sOutputDevice, unsigned int nSampleRate = 44100, unsigned int nChannels = 1, unsigned int nBlocks = 8, unsigned int nBlockSamples = 512)
+	{
+		Create(sOutputDevice, nSampleRate, nChannels, nBlocks, nBlockSamples);
+	}
+
+	~olcNoiseMaker()
+	{
+		Destroy();
+	}
+
+	bool Create(wstring sOutputDevice, unsigned int nSampleRate = 44100, unsigned int nChannels = 1, unsigned int nBlocks = 8, unsigned int nBlockSamples = 512)
+	{
+		m_bReady = false;
+		m_nSampleRate = nSampleRate;
+		m_nChannels = nChannels;
+		m_nBlockCount = nBlocks;
+		m_nBlockSamples = nBlockSamples;
+		m_nBlockFree = m_nBlockCount;
+		m_nBlockCurrent = 0;
+		m_pBlockMemory = nullptr;
+		m_pWaveHeaders = nullptr;
+
+		m_userFunction = nullptr;
+
+		// Validate device
+		vector<wstring> devices = Enumerate();
+		auto d = std::find(devices.begin(), devices.end(), sOutputDevice);
+		if (d != devices.end())
+		{
+			// Device is available
+			int nDeviceID = distance(devices.begin(), d);
+			WAVEFORMATEX waveFormat;
+			waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+			waveFormat.nSamplesPerSec = m_nSampleRate;
+			waveFormat.wBitsPerSample = sizeof(T) * 8;
+			waveFormat.nChannels = m_nChannels;
+			waveFormat.nBlockAlign = (waveFormat.wBitsPerSample / 8) * waveFormat.nChannels;
+			waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+			waveFormat.cbSize = 0;
+
+			// Open Device if valid
+			if (waveOutOpen(&m_hwDevice, nDeviceID, &waveFormat, (DWORD_PTR)waveOutProcWrap, (DWORD_PTR)this, CALLBACK_FUNCTION) != S_OK)
+				return Destroy();
+		}
+
+		// Allocate Wave|Block Memory
+		m_pBlockMemory = new T[m_nBlockCount * m_nBlockSamples];
+		if (m_pBlockMemory == nullptr)
+			return Destroy();
+		ZeroMemory(m_pBlockMemory, sizeof(T) * m_nBlockCount * m_nBlockSamples);
+
+		m_pWaveHeaders = new WAVEHDR[m_nBlockCount];
+		if (m_pWaveHeaders == nullptr)
+			return Destroy();
+		ZeroMemory(m_pWaveHeaders, sizeof(WAVEHDR) * m_nBlockCount);
+
+		// Link headers to block memory
+		for (unsigned int n = 0; n < m_nBlockCount; n++)
+		{
+			m_pWaveHeaders[n].dwBufferLength = m_nBlockSamples * sizeof(T);
+			m_pWaveHeaders[n].lpData = (LPSTR)(m_pBlockMemory + (n * m_nBlockSamples));
+		}
+
+		m_bReady = true;
+
+		m_thread = thread(&olcNoiseMaker::MainThread, this);
+
+		// Start the ball rolling
+		unique_lock<mutex> lm(m_muxBlockNotZero);
+		m_cvBlockNotZero.notify_one();
+
+		return true;
+	}
+
+	bool Destroy()
+	{
+		return false;
+	}
+
+	void Stop()
+	{
+		m_bReady = false;
+		m_thread.join();
+	}
+
+	// Override to process current sample
+	virtual FTYPE UserProcess(int nChannel, FTYPE dTime)
+	{
+		return 0.0;
+	}
+
+	FTYPE GetTime()
+	{
+		return m_dGlobalTime;
+	}
+
+	
+
+public:
+	static vector<wstring> Enumerate()
+	{
+		int nDeviceCount = waveOutGetNumDevs();
+		vector<wstring> sDevices;
+		WAVEOUTCAPS woc;
+		for (int n = 0; n < nDeviceCount; n++)
+			if (waveOutGetDevCaps(n, &woc, sizeof(WAVEOUTCAPS)) == S_OK)
+				sDevices.push_back(woc.szPname);
+		return sDevices;
+	}
+
+	void SetUserFunction(FTYPE(*func)(int, FTYPE))
+	{
+		m_userFunction = func;
+	}
+
+	FTYPE clip(FTYPE dSample, FTYPE dMax)
+	{
+		if (dSample >= 0.0)
+			return fmin(dSample, dMax);
+		else
+			return fmax(dSample, -dMax);
+	}
+
+
+private:
+	FTYPE(*m_userFunction)(int, FTYPE);
+
+	unsigned int m_nSampleRate;
+	unsigned int m_nChannels;
+	unsigned int m_nBlockCount;
+	unsigned int m_nBlockSamples;
+	unsigned int m_nBlockCurrent;
+
+	T* m_pBlockMemory;
+	WAVEHDR *m_pWaveHeaders;
+	HWAVEOUT m_hwDevice;
+
+	thread m_thread;
+	atomic<bool> m_bReady;
+	atomic<unsigned int> m_nBlockFree;
+	condition_variable m_cvBlockNotZero;
+	mutex m_muxBlockNotZero;
+
+	atomic<FTYPE> m_dGlobalTime;
+
+	// Handler for soundcard request for more data
+	void waveOutProc(HWAVEOUT hWaveOut, UINT uMsg, DWORD dwParam1, DWORD dwParam2)
+	{
+		if (uMsg != WOM_DONE) return;
+
+		m_nBlockFree++;
+		unique_lock<mutex> lm(m_muxBlockNotZero);
+		m_cvBlockNotZero.notify_one();
+	}
+
+	// Static wrapper for sound card handler
+	static void CALLBACK waveOutProcWrap(HWAVEOUT hWaveOut, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
+	{
+		((olcNoiseMaker*)dwInstance)->waveOutProc(hWaveOut, uMsg, dwParam1, dwParam2);
+	}
+
+	// Main thread. This loop responds to requests from the soundcard to fill 'blocks'
+	// with audio data. If no requests are available it goes dormant until the sound
+	// card is ready for more data. The block is fille by the "user" in some manner
+	// and then issued to the soundcard.
+	void MainThread()
+	{
+		m_dGlobalTime = 0.0;
+		FTYPE dTimeStep = 1.0 / (FTYPE)m_nSampleRate;
+
+		// Goofy hack to get maximum integer for a type at run-time
+		T nMaxSample = (T)pow(2, (sizeof(T) * 8) - 1) - 1;
+		FTYPE dMaxSample = (FTYPE)nMaxSample;
+		T nPreviousSample = 0;
+
+		while (m_bReady)
+		{
+			// Wait for block to become available
+			if (m_nBlockFree == 0)
+			{
+				unique_lock<mutex> lm(m_muxBlockNotZero);
+				while(m_nBlockFree == 0) // sometimes, Windows signals incorrectly
+					m_cvBlockNotZero.wait(lm);
+			}
+
+			// Block is here, so use it
+			m_nBlockFree--;
+
+			// Prepare block for processing
+			if (m_pWaveHeaders[m_nBlockCurrent].dwFlags & WHDR_PREPARED)
+				waveOutUnprepareHeader(m_hwDevice, &m_pWaveHeaders[m_nBlockCurrent], sizeof(WAVEHDR));
+
+			T nNewSample = 0;
+			int nCurrentBlock = m_nBlockCurrent * m_nBlockSamples;
+			
+			for (unsigned int n = 0; n < m_nBlockSamples; n+=m_nChannels)
+			{
+				// User Process
+				for (unsigned int c = 0; c < m_nChannels; c++)
+				{
+					if (m_userFunction == nullptr)
+						nNewSample = (T)(clip(UserProcess(c, m_dGlobalTime), 1.0) * dMaxSample);
+					else
+						nNewSample = (T)(clip(m_userFunction(c, m_dGlobalTime), 1.0) * dMaxSample);
+					
+					m_pBlockMemory[nCurrentBlock + n + c] = nNewSample;
+					nPreviousSample = nNewSample;
+				}
+				
+				m_dGlobalTime = m_dGlobalTime + dTimeStep;
+			}
+
+			// Send block to sound device
+			waveOutPrepareHeader(m_hwDevice, &m_pWaveHeaders[m_nBlockCurrent], sizeof(WAVEHDR));
+			waveOutWrite(m_hwDevice, &m_pWaveHeaders[m_nBlockCurrent], sizeof(WAVEHDR));
+			m_nBlockCurrent++;
+			m_nBlockCurrent %= m_nBlockCount;
+		}
+	}
+};
